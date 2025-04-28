659491d3cd5b6d561fe29e1214b7eac0e83d918d
Amadeu Rosa <164663678+amadeurosa@users.noreply.github.com>
2025-04-27 21:49:16 -0300

    Segunda altarecao de seguranca

--- backend/inserir_dados.py
+++ backend/inserir_dados.py
@@ -3,7 +3,7 @@ import mysql.connector
 conexao = mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Hauptbahnhof581988#",
+    password="-------------",
     database="projeto_ecosolar"
 )
 
